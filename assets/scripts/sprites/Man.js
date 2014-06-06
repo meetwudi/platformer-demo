f6266268bff7f62a6dfdad0f692fa45048b7c836
John Wu <webmaster@leapoahead.com>
2014-06-06 22:30:40 +0800

    add man

---
+++ assets/scripts/sprites/Man.js
@@ -0,0 +1,24 @@
+define(['kiwi'], function(K) {
+  function Man(state, x, y) {
+    K.GameObjects.Sprite.call(this, state, state.textures['man'], x, y, false);
+    // facing direction, either left or right
+    this.facing = 'right';
+    // add animations
+    this.animation.add('idle-right', [8], 0.1, false);
+    this.animation.add('idle-left', [4], 0.1, false);
+    this.animation.add('walk-right', [8, 9, 10, 11], 0.1, true);
+    this.animation.add('walk-left', [4, 5, 6, 7], 0.1, true);
+    // play default animation
+    this.animation.play('idle-' + this.facing);
+    // add ArcadePhysics support
+    this.physics = this.components.add(new K.Components.ArcadePhysics(this, this.box));
+    this.physics.acceleration = new K.Geom.Point(0, 9.8);
+  }
+  K.extend(Man, K.GameObjects.Sprite);
+
+  Man.prototype.update = function() {
+    K.GameObjects.Sprite.prototype.update.call(this);
+    this.physics.update();
+  };
+  return Man;
+});
--- assets/scripts/states/MainState.js
+++ assets/scripts/states/MainState.js
@@ -1,4 +1,4 @@
-define(['kiwi'], function(K) {
+define(['kiwi', 'sprites/Man'], function(K, Man) {
   var MainState = new K.State('MainState');
   MainState.preload = function() {
     // Rebuild all assets in order to have access to them
@@ -7,15 +7,22 @@ define(['kiwi'], function(K) {
   };
   MainState.create = function() {
     K.State.prototype.create.call(this);
-    createMap.call(this);
+    // Create the map
+    this.map = new K.GameObjects.Tilemap.TileMap(this, 'map_data', this.textures['map']);
+    this.addChild(this.map.layers[0]);
+    this.map.tileTypes.map(function(tileType, idx) {
+      if (idx > 0) {
+        tileType.allowCollisions = K.Components.ArcadePhysics.ANY;
+      }
+    });
+    // Create the player man!
+    this.man = new Man(this, 10, 400);
+    this.addChild(this.man);
   };
   MainState.update = function() {
     K.State.prototype.update.call(this);
+    // Resolve the collision between the map and the man
+    this.map.layers[0].physics.overlapsTiles(this.man, true);
   };
   return MainState;
-
-  function createMap() {
-    this.map = new K.GameObjects.Tilemap.TileMap(this, 'map_data', this.textures['map']);
-    this.addChild(this.map.layers[0]);
-  }
 });
