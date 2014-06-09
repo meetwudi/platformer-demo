411fc915e27073ac52222b06bf755974f3333999
John Wu <webmaster@leapoahead.com>
2014-06-09 22:55:47 +0800

    refactor

--- assets/scripts/sprites/Rock.js
+++ assets/scripts/sprites/Rock.js
@@ -1,6 +1,5 @@
 define(['kiwi'], function(K) {
   function Rock(state, x) {
-    console.log(state.textures['rock']); 
     K.GameObjects.Sprite.call(this, state, state.textures['rock'], x, -15, false);
     // add ArcadePhysics support
     this.physics = this.components.add(new K.Components.ArcadePhysics(this, this.box));
